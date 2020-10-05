1cbf6e8d9855e2b9e1f14935623714f6626b6a7a
Ewerechukwu Favour Asaka <61825878+ewerechukwu@users.noreply.github.com>
2020-10-05 14:44:33 -0700

    Create QUEUE1.py

---
+++ QUEUE1.py
@@ -0,0 +1,14 @@
+
+list=[]
+list.append(1) # append 1
+print("enqueue:",list)
+list.append(2) # append 2
+print("enqueue:",list)
+list.append(3) # append 3
+print("enqueue:",list)
+list.pop(0) # pop 1
+print("dequeue:",list)
+print("peek:",list[-1]) # get top most element
+list.pop(0) # pop 2
+print("dequeue:",list)
+print("peek:",list[-1]) # get top most element
